1180e3254dca589e195b8ab1d39ae184b4e4c9c3
evelyn <evelyncranston@Live.ca>
2017-10-08 12:18:05 -0700

    Select bus duplicate

--- kafo-mobile/App.js
+++ kafo-mobile/App.js
@@ -16,6 +16,9 @@ export default class App extends React.Component {
         <KafoTextInput />
 
         <KafoSelectBus />
+
+       < KafoSelectBus />
+
         <KafoButton />
         
         
