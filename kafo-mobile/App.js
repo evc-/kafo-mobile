cc8cd5a39e13085ddf5287fefd6f8465d6fa9963
evelyn <evelyncranston@Live.ca>
2017-10-08 13:42:44 -0700

    Changing App Pages

--- kafo-mobile/App.js
+++ kafo-mobile/App.js
@@ -10,26 +10,42 @@ import KafoResults from './kafo-results';
 
 
 export default class App extends React.Component {
+    
+     constructor(props) {
+        super(props);
+         //to change pages of the app 
+        this.state = {appState: 0};
+     }
+    
+    //a function to change pages in the app by updating "appState" - 0 for first page, 1 for second page, etc. 
+    //gets a parameter of pagenum, which is used to set the "appState" 
+    changeAppPage(pagenum){
+        this.setState({appState: pagenum});
+    }
+ 
+    
+    
   render() {
-    return (
-      <View style={styles.container}>
-        <KafoIcon  />
-        <KafoHeader headerText="TEST" />
-        <KafoTextInput />
-        <KafoSelectBus />
-
-       < KafoSelectBus />
-
-        <KafoButton />
-        <KafoMap />
-        <KafoResults />
-        
-        
-      </View>
-    );
+      
+      if (this.state.appState == 0){
+            return (
+                <View style={styles.container}>
+                    <KafoIcon  />
+                    <KafoHeader headerText="Which stop are you at?" />
+                    <KafoTextInput changePage={(pagenum) => this.changeAppPage(pagenum)} />
+                </View>
+            );
+          
+      } else {
+           return (
+                <View style={styles.container}>
+                    <KafoSelectBus />
+                    <KafoButton />
+                </View>
+            );
+      }
   }
 }
-
 
 
 const styles = StyleSheet.create({
--- kafo-mobile/kafo-textinput.js
+++ kafo-mobile/kafo-textinput.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import { AppRegistry, TextInput } from 'react-native';
 
 export default class KafoTextInput extends Component {
+    
   constructor(props) {
     super(props);
-    this.state = { text: 'Stop Id #' };
+    this.state = { text: '' };
+      
+    this.updateText = this.updateText.bind(this);
   }
+    
+    //a function to update the text with with whatever is entered 
+    //then we check the length of the entry and if its 5 characters then we set the "state" of the components text to be "test"
+    updateText(text){
+        
+        this.setState({text});
+        if (text.length == 5){
+            this.props.changePage(1);
+        }
+    }
 
   render() {
     return (
       <TextInput
         style={{height: 140, width: 300,  borderColor: 'gray'}}
-        onChangeText={(text) => this.setState({text})}
+        onChangeText={(text) => this.updateText(text)}
         value={this.state.text}
+        maxLength={5}
+        placeholder={"Enter Stop ID"}
+
       />
     );
   }
