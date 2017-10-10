1bef6074e677db68f652e3f71f7522e961165372
tiffanywgtang <tiffanywgtang@hotmail.com>
2017-10-09 22:07:21 -0700

    added maps

--- kafo-mobile/kafo-map.js
+++ kafo-mobile/kafo-map.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import MapView from 'react-native-maps';
 
 export default class KafoMap extends Component {
-  render() {
-    return (        
-        <View>
-            
+  constructor(props){
+      super(props);
+      
+  }
+    
+    render() {
+    const { region } = this.props;
+    console.log(region);
+
+    return (
+      <View style ={styles2.container}>
+        <MapView
+          style={styles2.map}
+        {
+        //the latt and the longg is when the user clicks the bus route and then it grabs the latitude and longitude from that original page, latt and longg are just the names I gave it
+        }
+        region={{
+            latitude: this.props.latt,
+            longitude: this.props.longg,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
       </View>
     );
   }
 }
+
+const styles2 = StyleSheet.create({
+  container: {
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+      width: 250,
+      height: 250,
+  },
+});
 
 
--- kafo-mobile/kafo-results.js
+++ kafo-mobile/kafo-results.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import MapJS from './kafo-maps.js';
 
 export default class KafoResults extends Component {
   render() {
     return (        
         <View>
-            
+        {
+            //this will display the map
+        }
+            <MapJS 
+        latt={this.state.lat}
+        longg = {this.state.lng}
+        />
+      </View>
       </View>
     );
   }
