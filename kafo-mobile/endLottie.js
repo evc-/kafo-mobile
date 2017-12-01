d0a9558473a481ae5da2115678e6a061bb7915fb
camillecamahort <camillecamahort@gmail.com>
2017-12-01 11:55:09 -0800

    Ending animation is on last modal

---
+++ kafo-mobile/endLottie.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Animation from 'lottie-react-native';
+
+
+import anim from './img/data.json';
+
+export default class endLottie extends Component {
+  componentDidMount() {
+    this.animation.play();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Animation
+            ref={animation => {
+              this.animation = animation;
+            }}
+            style={{
+              width: 300,
+              height: 200
+            }}
+            loop={true}
+            source={anim}
+          />
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff'
+  }
+});
