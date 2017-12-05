c61c536df478e59f664eca888b93180594ba8e44
evelyn <evelyncranston@Live.ca>
2017-12-05 00:13:05 -0800

    No open shops styling

--- kafo-mobile/coffeeResultsModal.js
+++ kafo-mobile/coffeeResultsModal.js
@@ -6,7 +6,7 @@ export default class CoffeeResultsModal extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            selectedTrip: 0
+            selectedTrip: 0,
         }    
         
         this.startTimer = this.startTimer.bind(this);
@@ -22,10 +22,6 @@ export default class CoffeeResultsModal extends React.Component {
     
     changeBusTime(choice){
         this.props.changeBusArrival(choice);
-//        this.setState({
-//            selectedTrip: choice
-//        })
-        
     }
     
     compareTimesToSort(shop1, shop2){
@@ -36,120 +32,115 @@ export default class CoffeeResultsModal extends React.Component {
     
 
 render() {
-    var selectedColor;
-    var noShops=null;
-    
-    if(this.props.getCoffeeShops.length == 0){
-        noShops = (
-            <View>
-            <Text>Opps! There's no coffee shops around you or there's none open right now!</Text>
-            </View>
-                  )
-    }
-    
+    var headerMsg;
     if(this.props.shopWithStatus){
         if (this.props.shopWithStatus.length > 1){
             this.props.shopWithStatus.sort(this.compareTimesToSort(this.props.shopWithStatus, this.props.shopWithStatus));
-        }
-            var shopInfo = this.props.shopWithStatus.map((currentValue, i, array)=> {
-               
-                return( 
-                 
-    <View key={i}>
-      <TouchableOpacity onPress={this.startTimer.bind(this, i)} style={i%2==1 ? styles.touchableStyle1 : styles.touchableStyle2}> 
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
-                <View style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', width: '100%'}}>
-                    <View style={(currentValue.status == 'statusGreen') ? styles.statusGreenStyle : ((currentValue.status == 'statusRed') ? styles.statusRedStyle : styles.statusOrangeStyle)}>                      
-                        <Image 
-                            source={require('./img/cup-icon-02.png')} 
-                            style={{width: 10, height: 10, alignSelf:'flex-start'}}
-                        />
-                    </View>
-                    <View style={{height: '100%', width: '100%'}}>
-                        <Text style={{width: '85%', fontSize: 28, color: '#303C45', textAlign: 'left', fontWeight: 'bold', paddingLeft: 10}} numberOfLines={1} ellipsizeMode={'tail'}>{currentValue.name}</Text>
-                        <Text style={{fontSize: 12, color: '#303C45', textAlign: 'left', fontWeight: 'bold', paddingLeft: 10}}>Bus arrives in {currentValue.nextBus} min{"\n"}</Text>
-                        <Text style={{fontSize: 12, color: '#303C45', textAlign: 'left', paddingLeft: 10, paddingBottom: 10}}>Trip time is {currentValue.journeyTime} min</Text>
-                    </View>
-    
-                </View>
-                <View>
-                    <TouchableOpacity onPress={this.startTimer.bind(this, i)} style={{alignSelf:'flex-end', height: '100%', justifyContent: 'center'}}>
-                        <Image 
-                            source={require('./img/arrow-01.png')} 
-                            style={{width: 40, height: 40, alignSelf: 'flex-end'}}
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </TouchableOpacity>
-    </View>
-    );
-
-
-
-})}
-
-var arrivalChoices; 
-
-if (this.props.selectedBusState){
-    var mtime0 = this.props.selectedBusState.Schedules[0].ExpectedLeaveTime.split(" ");
-    var datestr0 = mtime0[0];
- 
-    var mtime1 = this.props.selectedBusState.Schedules[1].ExpectedLeaveTime.split(" ");
-    var datestr1 = mtime1[0];
- 
-    var mtime2 = this.props.selectedBusState.Schedules[2].ExpectedLeaveTime.split(" ");
-    var datestr2 = mtime2[0];
- 
-    
-    arrivalChoices = 
-         <View style={{backgroundColor:'#42565E', flexDirection: 'row', justifyContent: 'space-between'}}>
-                            <View style={{flex: 4}}>
-                                <Text style={{color: 'white', fontWeight: 'bold', paddingLeft: 5}}>Bus {this.props.selectedBusState.RouteNo}</Text>
-                            </View>
-                            <View style={{flex: 4}}>
-                                <TouchableOpacity onPress={() => this.changeBusTime(0)} style={{backgroundColor:(this.props.busArrivalChoice === 0)?'#6fa7a8':'#42565E'}}> 
-                                    <Text style={{color: 'white', textAlign: 'center'}}>
-                                        {datestr0}
-                                    </Text> 
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{flex: 4}}>
-                                <TouchableOpacity onPress={() => this.changeBusTime(1)} style={{backgroundColor:(this.props.busArrivalChoice === 1)?'#6fa7a8':'#42565E'}}> 
-                                    <Text style={{color: 'white', textAlign: 'center'}}>
-                                        {datestr1}
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{flex: 4}}>
-                                <TouchableOpacity onPress={() => this.changeBusTime(2)} style={{backgroundColor:(this.props.busArrivalChoice === 2)?'#6fa7a8':'#42565E'}}>
-                                    <Text style={{color: 'white', textAlign: 'center'}}>
-                                        {datestr2}
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                    </View>
-;
-}
-
-    return (
-       
-        <View style={{flex:1}} >
+        } 
         
+        if (this.props.shopWithStatus.length == 0){
+            headerMsg ="Oh no!";
+            shopInfo = (
+                <View style={{alignItems:'center'}}>
+                <Text style={styles.question2Style}>There are no open coffee shops around you right now. 
+                {"\n"}
+                <Image 
+                    source={require('./img/heart-01.png')}
+                    style={{width: 100, height: 100}}
+                 />
+                {"\n"}
+                Don't panic - Try again in a different place, or tomorrow morning. If there's coffee around, you'll be the first to know.</Text>
+                </View>
+            )
+        } else {
+            headerMsg ="Time for coffee at these shops";
+           var shopInfo = this.props.shopWithStatus.map((currentValue, i, array)=> {
+                return(  
+                    <View key={i}>
+                      <TouchableOpacity onPress={this.startTimer.bind(this, i)} style={i%2==1 ? styles.touchableStyle1 : styles.touchableStyle2}> 
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                                <View style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', width: '100%'}}>
+                                    <View style={(currentValue.status == 'statusGreen') ? styles.statusGreenStyle : ((currentValue.status == 'statusRed') ? styles.statusRedStyle : styles.statusOrangeStyle)}>                      
+                                        <Image 
+                                            source={require('./img/cup-icon-02.png')} 
+                                            style={{width: 10, height: 10, alignSelf:'flex-start'}}
+                                        />
+                                    </View>
+                                    <View style={{height: '100%', width: '100%'}}>
+                                        <Text style={{width: '85%', fontSize: 28, color: '#303C45', textAlign: 'left', fontWeight: 'bold', paddingLeft: 10}} numberOfLines={1} ellipsizeMode={'tail'}>{currentValue.name}</Text>
+                                        <Text style={{fontSize: 12, color: '#303C45', textAlign: 'left', fontWeight: 'bold', paddingLeft: 10}}>Bus arrives in {currentValue.nextBus} min{"\n"}</Text>
+                                        <Text style={{fontSize: 12, color: '#303C45', textAlign: 'left', paddingLeft: 10, paddingBottom: 10}}>Trip time is {currentValue.journeyTime} min</Text>
+                                    </View>
+
+                                </View>
+                                <View>
+                                    <TouchableOpacity onPress={this.startTimer.bind(this, i)} style={{alignSelf:'flex-end', height: '100%', justifyContent: 'center'}}>
+                                        <Image 
+                                            source={require('./img/arrow-01.png')} 
+                                            style={{width: 40, height: 40, alignSelf: 'flex-end'}}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                );
+            });
+        }
+    }
+
+    var arrivalChoices; 
+    if (this.props.selectedBusState && this.props.getCoffeeShops.length > 0){
+        var mtime0 = this.props.selectedBusState.Schedules[0].ExpectedLeaveTime.split(" ");
+        var datestr0 = mtime0[0];
+
+        var mtime1 = this.props.selectedBusState.Schedules[1].ExpectedLeaveTime.split(" ");
+        var datestr1 = mtime1[0];
+
+        var mtime2 = this.props.selectedBusState.Schedules[2].ExpectedLeaveTime.split(" ");
+        var datestr2 = mtime2[0];
+        
+        arrivalChoices = 
+             <View style={{backgroundColor:'#42565E', flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <View style={{flex: 4}}>
+                                    <Text style={{color: 'white', fontWeight: 'bold', paddingLeft: 5}}>Bus {this.props.selectedBusState.RouteNo}</Text>
+                                </View>
+                                <View style={{flex: 4}}>
+                                    <TouchableOpacity onPress={() => this.changeBusTime(0)} style={{backgroundColor:(this.props.busArrivalChoice === 0)?'#6fa7a8':'#42565E'}}> 
+                                        <Text style={{color: 'white', textAlign: 'center'}}>
+                                            {datestr0}
+                                        </Text> 
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={{flex: 4}}>
+                                    <TouchableOpacity onPress={() => this.changeBusTime(1)} style={{backgroundColor:(this.props.busArrivalChoice === 1)?'#6fa7a8':'#42565E'}}> 
+                                        <Text style={{color: 'white', textAlign: 'center'}}>
+                                            {datestr1}
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={{flex: 4}}>
+                                    <TouchableOpacity onPress={() => this.changeBusTime(2)} style={{backgroundColor:(this.props.busArrivalChoice === 2)?'#6fa7a8':'#42565E'}}>
+                                        <Text style={{color: 'white', textAlign: 'center'}}>
+                                            {datestr2}
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                        </View>
+    }
+    return (
+        <View style={{flex:1}}>
             <View>
-                <KafoHeader innerText={"Time for coffee at these shops"}/>
+                <KafoHeader innerText={headerMsg}/>
             </View>
             <View>
                 {arrivalChoices}
             </View>
             <ScrollView style={{flex:1}}>
                 {shopInfo}
-                {noShops}
             </ScrollView>
-        
         </View>
-    
-    )
+    );
   }
 }
 
@@ -194,5 +185,12 @@ const styles = StyleSheet.create({
         width: '100%',
         backgroundColor: '#F7F7F7',
         justifyContent: 'center',
-    }
+    },
+    
+    question2Style:{
+        textAlign: 'center',
+        padding:15,
+        fontSize: 15,
+        color: 'grey'
+      },
 });
